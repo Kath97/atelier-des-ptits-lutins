cb801da1952ccf6efbe5b98961821f45ae2e34ce
Kath97 <kathleen.malialin@gmail.com>
2020-04-21 17:31:54 -0300

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>L'Atelier des P'tits Lutins</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                L'Atelier des P'tits Lutins
                 </div>
 
                 <div class="links">
