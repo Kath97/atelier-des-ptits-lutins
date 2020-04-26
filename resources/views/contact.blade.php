2a8f33dcc5cbf7b4578ce192b9d41fb12141095c
Kath97 <kathleen.malialin@gmail.com>
2020-04-26 10:48:42 -0300

    tidy-style.css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,7 @@
 <div id="bloc-contact" class="min-height bg-contact">
 <h1 class="text-center">atelierdesptitslutins@gmail.com</h1>
 <p class="text-center">0694.28.10.94 &#8226; 3, avenue des petits lutins 97351 Rainbowtown</p>
-<div id="icones" class="text-center">
+<div id="social-bar" class="text-center">
       <a href="#" class="fa fa-facebook"></a>
       <a href="#" class="fa fa-instagram"></a>
       <a href="#" class="fa fa-pinterest"></a>
